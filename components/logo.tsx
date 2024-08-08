f9ece16c606d8e2b6b9aafd2ff2146538c617537
Léo Jansen <leo@leoo.app>
2024-08-07 23:55:00 -0300

    Update logo.tsx

--- components/logo.tsx
+++ components/logo.tsx
@@ -5,9 +5,11 @@ import React from 'react'
 const Logo = () => {
   return (
     <Link href="/">
-        <div>
+        <div className='hover: opacity-75 transition items-center gap-x-2 hidden md:flex'>
             <Image src="/logo.svg" alt="Taskify" width={30} height={30} />
-            <p>Logo</p>
+            <p className='text-lg text-neutral-700 pb-1'>
+                Taskify
+                </p>
         </div>
     </Link>
   )
