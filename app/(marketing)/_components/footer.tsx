2bfce1e76e5204a4e083b83c6d908bb682a0cd20
Léo Jansen <leo@leonardojansen.com>
2024-08-12 18:53:32 -0300

    Inserts footer

--- app/(marketing)/_components/footer.tsx
+++ app/(marketing)/_components/footer.tsx
@@ -3,24 +3,24 @@ import { Button } from '@/components/ui/button'
 import Link from 'next/link'
 import React from 'react'
 
-export const Navbar = () => {
+export const Footer = () => {
   return (
-    <div className='fixed top-0 w-full h-14 px-4 border-b shadow-sm bg-white flex items-center'>
+    <div className='fixed bottom-0 w-full  px-4 border-t  bg-slate-100'>
       <div className='md:max-w-screen-2xl mx-auto flex justify-between w-full items-center'>
        <Logo />
        <div className="space-x-4 md:block md:w-auto flex items-center justify-between w-full">
-        <Button size="sm" variant="outline" asChild>
+        <Button size="sm" variant="ghost" asChild>
           <Link href="/sign-in">
-          Login
-
+          Privacy Policy
+g
           </Link>
           
         </Button>
         
         
-          <Button size="sm" asChild>
+          <Button size="sm" variant="ghost" asChild>
             <Link href="/sign-up">
-            Get Taskify for free
+            Terms of Service
             </Link>
             </Button>
 
