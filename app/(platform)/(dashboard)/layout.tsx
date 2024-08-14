f8e1017d4c485724702352551ad1b97cbdf03dae
Léo Jansen <leo@leonardojansen.com>
2024-08-14 19:08:56 -0300

    Implements organizartions and Nav

---
+++ app/(platform)/(dashboard)/layout.tsx
@@ -0,0 +1,16 @@
+import { Navbar } from "./_components/Navbar";
+import React from "react";
+
+const DashboardLayout = (
+    {children} : {    children: React.ReactNode})=>{
+        return(
+            <div className="h-full w-full">
+                <Navbar/>
+                {children}
+            </div>
+        )
+
+    };
+
+
+export default DashboardLayout;
--- app/(platform)/protected/page.tsx
+++ app/(platform)/protected/page.tsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import { currentUser } from '@clerk/nextjs/server';
+import { UserButton } from '@clerk/nextjs';
 
 const ProtectedPage =  async () => {
   const user = await currentUser();
 
   return (
-    <div>User: {user?.firstName} </div>
+    <div><UserButton/> button</div>
   )
 }
 
