542a0ce622738f0bd873b1568f6618ec6d14cf8b
Léo Jansen <leo@leonardojansen.com>
2024-08-28 18:39:41 -0300

    Tests DB connection

--- app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
@@ -1,12 +1,32 @@
+import { db } from "@/lib/db";
 
 
 
 const OrganizationIdPage = () => {
+  async function create(formData:FormData) {
+    "use server"
 
+    const title = formData.get("title") as string;
+
+    await db.board.create({
+      data: {
+        title
+      }
+    })
+    
+  }
 
   return (
     <div>
-      Organization PAge
+      <form action={create}>
+        <input
+        id="title"
+        name="title"
+        required
+        placeholder="Enter a board title"
+        className="border border-black p-1"
+        />
+      </form>
       </div>
   )
 }
