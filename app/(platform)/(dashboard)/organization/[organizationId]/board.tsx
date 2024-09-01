609c4727ab33b7e37a5b8b96956a4ab405644f03
Léo Jansen <leo@leoo.app>
2024-09-01 15:01:54 -0300

    Implements Deleteboard

--- app/(platform)/(dashboard)/organization/[organizationId]/board.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/board.tsx
@@ -1,3 +1,6 @@
+import { deleteBoard } from "@/actions/deleteBoard";
+import { Button } from "@/components/ui/button";
+
 interface BoardProps {
     title: string;
     id: string;
@@ -7,10 +10,14 @@ const Board = ({
     title,
     id
 } : BoardProps ) => {
+    const deleteBoardWithId = deleteBoard.bind(null,id);
+
     return (
-        <div >
-            Board Title: {title}
-        </div>
+        <form className="flex items-center gap-x-2" action={deleteBoardWithId}>
+            <p>Board Title: {title}</p>
+            <Button variant="destructive" size="sm" type="submit">Delete</Button>
+            
+        </form>
     )
 }
 
