7249ead228007ac12756d77a84afdd8961b54050
Léo Jansen <leo@leoo.app>
2024-09-01 16:52:11 -0300

    implements validator

---
+++ app/(platform)/(dashboard)/organization/[organizationId]/form.tsx
@@ -0,0 +1,42 @@
+"use client"
+
+import { create } from "@/actions/createBoard"
+import { Button } from "@/components/ui/button"
+import { useActionState } from "react"
+import { useFormState } from "react-dom"
+
+
+export const Form = () => {
+    const initialState = { message: "", errors: {} }
+
+    const [state, dispatch] = useFormState(create, initialState);
+
+    return (
+        <form action={dispatch}>
+            <div className="flex flex-col space-y-2">
+                <input
+                    id="title"
+                    name="title"
+                    required
+                    placeholder="Enter a board title"
+                    className="border border-black p-1"
+                />
+                {state?.errors?.title ? (
+                    <div>
+                        {state.errors.title.map((error: string) => (
+                            <p key={error} className="text-rose-500">
+                                {error}
+
+                            </p>
+                        ))}
+                    </div>
+                ) : null}
+
+            </div>
+
+            <Button type="submit">
+                Submit
+            </Button>
+        </form>
+    )
+}
