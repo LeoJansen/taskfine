1ca9fcdd067dcf1a4a71a4637275258c2e4be3b4
Léo Jansen <leo@leoo.app>
2024-09-01 23:23:26 -0300

    Update create-safe-action.ts

--- lib/create-safe-action.ts
+++ lib/create-safe-action.ts
@@ -9,3 +9,13 @@ export type ActionState<TInput, TOutput> = {
     error?: string | null;
     data?: TOutput;
 }
+
+export const createSafeAction = <TInput, TOutput> (
+    schema: z.Schema<TInput>,
+    handler: (validateData: TInput) => Promise<ActionState<TInput, TOutput>>
+) => {
+    return async (data: TInput): Promise<ActionState<TInput, TOutput>> => {
+        const validationResult = schema.safeParse(data);
+
+    }
+}
