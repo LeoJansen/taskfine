4b90329165b30152aa3277e2df702ba0cfab8436
Léo Jansen <leo@leonardojansen.com>
2024-09-04 14:46:09 -0300

    Implements board-list

--- app/(platform)/(dashboard)/organization/[organizationId]/_components/info.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/_components/info.tsx
@@ -1,17 +1,18 @@
 "use client"
 
+import { Skeleton } from "@/components/ui/skeleton";
 import { useOrganization } from "@clerk/nextjs";
 import { CreditCard } from "lucide-react";
 import Image from "next/image";
 
 
 
-const Info = () => {
+export const Info = () => {
     const { organization, isLoaded } = useOrganization();
 
     if (!isLoaded) {
         return (
-            <p>Loading...</p>
+            <Info.Skeleton/>
         )
     }
 
@@ -42,9 +43,20 @@ const Info = () => {
 Info.Skeleton = function SkeletonInfo() {
     return(
         <div className="flex items-center gap-x-4">
+            <div className="w-[60px] h-[60px] relative">
+                <Skeleton className="w-full h-full absolute"/>
+            </div>
+            <div className="space-y-2">
+                <Skeleton className="h-10 w-[200px]"/>
+            </div>
+            <div className="flex items-center">
+                <Skeleton className="h-4 w-4 mr-2"/>
+                <Skeleton className="h-4 w-[100px]"/>
+
+
+            </div>
 
         </div>
     )
 }
 
-export default Info;
--- app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
@@ -1,4 +1,7 @@
-import Info from "./_components/info";
+import { Separator } from "@/components/ui/separator";
+import { Info } from "./_components/info";
+import { BoardList } from "./_components/board-list";
+
 
 
 
@@ -7,11 +10,12 @@ const OrganizationIdPage = async () => {
 
   return (
     <div className="w-full mb-20">
-      <Info/>
-   
-
-    
+      <Info />
+      <Separator className="my-4" />
+      <div className="px-2 md:px-4">
+        <BoardList/>
       </div>
+    </div>
   )
 }
 
