8685699be04301873c3d2138e6b675c0176d2706
Léo Jansen <leo@leoo.app>
2024-09-08 21:53:18 -0300

    Update layout.tsx

--- app/(platform)/(dashboard)/board/[boardId]/layout.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/layout.tsx
@@ -1,16 +1,24 @@
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
+
 const BoardIdLayout = ({
     children
 }: {
-    children: React.ReactNode
+    children: React.ReactNode,
+    params: { boardId: string }
 }) => {
+    const { orgId } = auth();
+
+    if (!orgId) {
+        redirect("/select-org");
+    };
 
     return (
         <div>
             <main className="relative pt-28 h-full">
-            {children}
-
+                {children}
             </main>
-            
+
         </div>
     )
 
