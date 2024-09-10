69feec0b375f4576b258fe041be466710fcde145
Léo Jansen <leo@leoo.app>
2024-09-10 11:29:08 -0300

    Update board-options.tsx

--- app/(platform)/(dashboard)/board/[boardId]/_components/board-options.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_components/board-options.tsx
@@ -1,19 +1,32 @@
 "use client"
 
 
-import { Popover,
-    
- } from "@radix-ui/react-popover";
+import { Button } from "@/components/ui/button";
+
+import { Popover, PopoverTrigger, PopoverContent } from "@radix-ui/react-popover";
+import { MoreHorizontal } from "lucide-react";
 interface BoardOptionsProps {
-    id:string;
+    id: string;
 }
 
-export const BoardOptions = ({id})=> {
+export const BoardOptions = ({ id }) => {
 
-    return(
-        <div className="text-white">
-            Options
-        </div>
+    return (
+        <Popover>
+            <PopoverTrigger asChild>
+                <Button className="h-auto w-auto p-2" variant="transparent">
+                    <MoreHorizontal className="h-4 w-4" />
+                </Button>
+            </PopoverTrigger>
+            <PopoverContent
+            className="px-0 pt-3 pb-3"
+            side="bottom"
+            align="start"
+            >
+
+            </PopoverContent>
+
+        </Popover>
     )
 
 }
