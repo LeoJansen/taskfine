8032dd32d1fb2135bf26d8b87b2fbde34b37eda6
Léo Jansen <leo@leonardojansen.com>
2024-09-10 17:31:33 -0300

    Implements delete action

--- actions/delete-board/index.ts
+++ actions/delete-board/index.ts
@@ -5,38 +5,37 @@ import { InputType, ReturnType } from "./types";
 import { db } from "@/lib/db";
 import { revalidatePath } from "next/cache";
 import { createSafeAction } from "@/lib/create-safe-action";
-import { UpdateBoard } from "./schema";
+import { DeleteBoard } from "./schema";
+import { redirect } from "next/navigation";
 
 const handler = async (data: InputType): Promise<ReturnType> => {
-    const {userId, orgId} = auth();
-    if(!userId || !orgId) {
+
+    const { userId, orgId } = auth();
+    
+    if (!userId || !orgId) {
         return {
             error: "Unauthorized"
         };
     };
 
-    const {title, id} = data;
+    const { id } = data;
     let board;
 
     try {
-        board= await db.board.update({
+        board = await db.board.delete({
             where: {
                 id,
                 orgId
             },
-            data: {
-                title
-            },
         });
-        
+
     } catch (error) {
         return {
-            error: "Failed to update"
-        }   
+            error: "Failed to delete"
+        }
     };
-    revalidatePath(`/board/${id}`)
-    return { data: board};
-
+    revalidatePath(`/organization/${orgId}`);
+    redirect(`/organization/${orgId}`);
 };
 
-export const updateBoard = createSafeAction(UpdateBoard, handler);
+export const deleteBoard = createSafeAction(DeleteBoard, handler);
