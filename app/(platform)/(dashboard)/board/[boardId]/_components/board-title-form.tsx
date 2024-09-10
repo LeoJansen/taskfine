8d390ffe65164d8aa713236db65648a03926e73b
Léo Jansen <leo@leoo.app>
2024-09-09 22:46:01 -0300

    Update board-title-form.tsx

--- app/(platform)/(dashboard)/board/[boardId]/_components/board-title-form.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_components/board-title-form.tsx
@@ -30,7 +30,8 @@ export const BoardTitleForm = ({
             setTitle(data.title);
             disableEditing();
         },
-        onError: () => {
+        onError: (error) => {
+            toast.error(error);
 
         }
     })
