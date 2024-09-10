04d3e358c30e2b61ee8e33867e62d20c938a4457
Léo Jansen <leo@leoo.app>
2024-09-09 22:50:22 -0300

    Updates Board Options

---
+++ app/(platform)/(dashboard)/board/[boardId]/_components/board-options.tsx
@@ -0,0 +1,15 @@
+"use client"
+
+interface BoardOptionsProps {
+    id:string;
+}
+
+export const BoardOptions = ({id})=> {
+
+    return(
+        <div className="text-white">
+            Options
+        </div>
+    )
+
+}
