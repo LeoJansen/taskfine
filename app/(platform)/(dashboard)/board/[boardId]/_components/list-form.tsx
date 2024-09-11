10462ba8b2259281bf0529205c6f4e50131149be
Léo Jansen <leo@leoo.app>
2024-09-11 01:47:41 -0300

    Update list-form.tsx

--- app/(platform)/(dashboard)/board/[boardId]/_components/list-form.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_components/list-form.tsx
@@ -3,15 +3,21 @@ import { ListWrapper } from "./list-wrapper";
 import { useState, useRef, ElementRef } from "react";
 import { useEventListener, useOnClickOutside } from "usehooks-ts";
 import { FormInput } from "@/components/form/form-input";
-import { useParams } from "next/navigation";
+import { useParams, useRouter } from "next/navigation";
 import { FormSubmit } from "@/components/form/form-button";
 import { Button } from "@/components/ui/button";
+import { useAction } from "@/hooks/use-action";
+import { createList } from "@/actions/create-list";
+import { toast } from "sonner";
+
+
 
 export const ListForm = () => {
     const [isEditing, setIsEditing] = useState<boolean>(false);
     const formRef = useRef<ElementRef<"form">>(null);
     const inputRef = useRef<ElementRef<"input">>(null);
     const params = useParams();
+    const router = useRouter();
 
     const enableEditing = () => {
         setIsEditing(true);
@@ -24,6 +30,17 @@ export const ListForm = () => {
         setIsEditing(false);
     };
 
+    const {execute, fieldErrors} = useAction(createList,{
+        onSuccess: (data) => {
+            toast.success(`List "${data.title}" created`);
+            disableEditing();
+            router.refresh();
+        },
+        onError: (error) => {
+            toast.error(error);
+        }
+    })
+
 
     const onKeyDown = (e: KeyboardEvent) => {
         if (e.key === "Escagpe") {
@@ -33,15 +50,27 @@ export const ListForm = () => {
     useEventListener("keydown", onKeyDown);
     useOnClickOutside(formRef, disableEditing);
 
+    const onSubmit = (formData: FormData) => {
+        const title = formData.get("title") as string;
+        const boardId = formData.get("boardId") as string;
+
+        execute({
+            title,
+            boardId
+        })
+    }
+
     if (isEditing) {
         return (
             <ListWrapper>
                 <form
+                action={onSubmit}
                     ref={formRef}
                     className="w-full p-3 rounded-md bg-white space-y-4 shadow-md"
                 >
                     <FormInput
                     ref={inputRef}
+                    errors={fieldErrors}
                     id="title"
                     className="text-sm px-2 py-1 h-7 font-medium border-transparent hover:border-input focus:border-input transition"
                     placeholder="Enter a list title"
