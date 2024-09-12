1c62a3c01dec38958ecff6b97c1c27a91467d44d
Léo Jansen <leo@leoo.app>
2024-09-12 01:11:15 -0300

    Update index.ts

--- actions/create-card/index.ts
+++ actions/create-card/index.ts
@@ -5,7 +5,7 @@ import { InputType, ReturnType } from "./types";
 import { db } from "@/lib/db";
 import { revalidatePath } from "next/cache";
 import { createSafeAction } from "@/lib/create-safe-action";
-import { CreateList } from "./schema";
+import { CreateCard } from "./schema";
 
 const handler = async (data: InputType): Promise<ReturnType> => {
     const {userId, orgId} = auth();
@@ -16,48 +16,18 @@ const handler = async (data: InputType): Promise<ReturnType> => {
     };
 
     const {title, boardId} = data;
-    let list;
+    let card;
 
     try {
-        const board = await db.board.findUnique({
-            where: {
-                id: boardId,
-                orgId
-            },
-        });
-
-        if(!board){
-            return {
-                error:"Board not found",
-            };
-        };
-
-
-        const lastList = await db.list.findFirst({
-            where: {boardId},
-            orderBy: {order: "desc"},
-            select: {order: true}
-        })
-
-        const newOrder = lastList ? lastList.order + 1 : 1;
-
-        list= await db.list.create({
-         data: {
-            title,
-            boardId,
-            order: newOrder
-         }
        
-        });
-        
     } catch (error) {
         return {
             error: "Failed to create"
         }   
     };
     revalidatePath(`/board/${boardId}`)
-    return { data: list};
+    return { data: card};
 
 };
 
-export const createList = createSafeAction(CreateList, handler);
+export const createCard = createSafeAction(CreateCard, handler);
