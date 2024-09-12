f279888cc17ce57e0b31a856986ca8df35823464
Léo Jansen <leo@leoo.app>
2024-09-12 01:09:51 -0300

    Update types.ts

--- actions/create-card/types.ts
+++ actions/create-card/types.ts
@@ -1,9 +1,9 @@
 import { z } from "zod";
-import { CreateList} from "./schema";
+import { CreateCard} from "./schema";
 import { ActionState } from "@/lib/create-safe-action";
-import { List } from "@prisma/client";
+import { Card } from "@prisma/client";
 
 
 
-export type InputType = z.infer<typeof CreateList>;
-export type ReturnType = ActionState<InputType, List>;
+export type InputType = z.infer<typeof CreateCard>;
+export type ReturnType = ActionState<InputType, Card>;
