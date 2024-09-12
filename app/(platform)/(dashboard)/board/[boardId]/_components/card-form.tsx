e8d308fd0a1e3452ff6b253c9314376517d4921f
Léo Jansen <leo@leonardojansen.com>
2024-09-12 13:18:37 -0300

    Update card-form.tsx

--- app/(platform)/(dashboard)/board/[boardId]/_components/card-form.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_components/card-form.tsx
@@ -48,23 +48,30 @@ export const CardForm = forwardRef<HTMLTextAreaElement, CardFormProps>(({
         if (e.key === "Enter" && !e.shiftKey) {
             e.preventDefault();
             formRef.current?.requestSubmit()
+        };
+    };
 
-        }
-    }
+    const onSubmit = (formData: FormData) => {
+        const title = formData.get("title") as string;
+        const listId = formData.get("listId") as string;
+        const boardId = formData.get("boardId") as string;
+
+        execute({title, listId, boardId});
+
+    };
 
 
     if (isEditing) {
         return (
             <form
                 className="m-1 py-0.5 px-1 space-y-4"
-
             >
                 <FormTextarea
                     id="title"
-                    onKeyDown={() => { }}
+                    onKeyDown={onTextareakeyDown}
                     ref={ref}
                     placeholder="Enter a title for this card"
-                    errors={{}}
+                    errors={fieldErrors}
                 />
                 <input
                     hidden
