1f7f426a7cbce2fe707f3e927b8d3cbdd2977fac
Léo Jansen <leo@leoo.app>
2024-09-12 01:03:56 -0300

    Update footer.tsx

--- app/(marketing)/_components/footer.tsx
+++ app/(marketing)/_components/footer.tsx
@@ -7,26 +7,23 @@ export const Footer = () => {
   return (
     <div className='fixed bottom-0 w-full  p-4 border-t  bg-slate-100'>
       <div className='md:max-w-screen-2xl mx-auto flex justify-between w-full items-center'>
-       <Logo />
-       <div className="space-x-4 md:block md:w-auto flex items-center justify-between w-full">
-        <Button size="sm" variant="ghost" asChild>
-          <Link href="/sign-in">
-          Privacy Policy
-g
-          </Link>
-          
-        </Button>
-        
-        
+        <Logo />
+        <div className="space-x-4 md:block md:w-auto flex items-center justify-between w-full">
+          <Button size="sm" variant="ghost" asChild>
+            <Link href="/sign-in">
+              Privacy Policy
+            </Link>
+          </Button>
+
           <Button size="sm" variant="ghost" asChild>
             <Link href="/sign-up">
-            Terms of Service
+              Terms of Service
             </Link>
-            </Button>
+          </Button>
 
-       </div>
         </div>
-        </div>
-  )
-}
+      </div>
+    </div>
+  );
+};
 
