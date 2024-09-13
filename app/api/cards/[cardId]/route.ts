6a0bd6a54290821d65c3e6c98c609c397cca92d6
Léo Jansen <leo@leoo.app>
2024-09-13 20:54:12 -0300

    Update route.ts

--- app/api/cards/[cardId]/route.ts
+++ app/api/cards/[cardId]/route.ts
@@ -1,3 +1,4 @@
+import { db } from "@/lib/db";
 import { auth } from "@clerk/nextjs/server";
 import { NextResponse } from "next/server";
 
@@ -11,6 +12,26 @@ export async function GET(
         if (!userId || !orgId) {
             return new NextResponse("Unauthorized", { status: 401 });
         }
+
+        const card = await db.card.findUnique({
+            where: {
+                id: params.cardId,
+                list: {
+                    board: {
+                        orgId,
+                    },
+                },
+            },
+            include: {
+                list: {
+                    select: {
+                        title: true,
+                    },
+                },
+            },
+        });
+
+        return NextResponse.json(card);
 
     } catch (error) {
         return new NextResponse("Internal Error", { status: 500 });
