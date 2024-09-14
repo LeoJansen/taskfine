f65937d055fc3f68ca92875b6fbff9a35c93f094
Léo Jansen <leo@leoo.app>
2024-09-14 03:21:44 -0300

    Update schema.ts

--- actions/update-card/schema.ts
+++ actions/update-card/schema.ts
@@ -1,7 +1,17 @@
 import { z } from "zod";
 
 
-export const UpdateBoard = z.object({
+export const UpdateCard = z.object({
+    boardId: z.string(),
+    description: z.optional(
+        z.string({
+            required_error: "Description is required",
+            invalid_type_error: "Description is required",
+        })
+        .min(3, {
+            message: "Description is too short",
+        }),
+    ),
     title: z.string({
         required_error: "Title is required",
         invalid_type_error: "Title is required",
