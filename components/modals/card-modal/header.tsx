f2e99e4068f709cec7fde03f1320de6a999823f1
Léo Jansen <leo@leoo.app>
2024-09-14 02:45:06 -0300

    Update header.tsx

--- components/modals/card-modal/header.tsx
+++ components/modals/card-modal/header.tsx
@@ -1,7 +1,9 @@
 "use client";
 
+import { FormInput } from "@/components/form/form-input";
 import { CardWithList } from "@/types";
-import { Layout } from "lucide-react";
+import {  Layout } from "lucide-react";
+import { useRef, useState } from "react";
 
 interface HeaderProps {
     data: CardWithList;
@@ -10,9 +12,21 @@ interface HeaderProps {
 export const Header = ({
     data
 }: HeaderProps) => {
+    const inputRef = useRef()
+    const [title, setTitle] = useState(data?.title);
+
+
     return (
         <div className="flex items-center gap-x-3 mb-6 w-full">
              <Layout className="h-5 w-5 mt-1 text-neutral-700"/>
+             <div className="w-full">
+                <FormInput
+                id="title"
+                ref={inputRef}
+                defaultValue={title}
+                />
+
+             </div>
         </div>
     );
 };
