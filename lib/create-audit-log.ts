f481411fbec2869ca161ff37ba9ee6f5c1dbaa6b
Léo Jansen <leo@leoo.app>
2024-09-15 13:34:43 -0300

    Creates create-audit-log

---
+++ lib/create-audit-log.ts
@@ -0,0 +1,42 @@
+import { auth, currentUser } from "@clerk/nextjs/server";
+import { ACTION, ENTITY_TYPE } from "@prisma/client";
+import { db } from "./db";
+
+
+interface CreateAuditLogProps {
+    entityId: string;
+    entityType: ENTITY_TYPE;
+    entityTitle: string;
+    action: ACTION;
+};
+
+export const CreateAuditLog = async ({
+    entityId,
+    entityType,
+    entityTitle,
+    action
+}: CreateAuditLogProps) => {
+
+    try {
+        const { orgId } = auth();
+        const user = await currentUser();
+
+        if(!user || !orgId) {
+            throw new Error("User not found!");
+        };
+
+        await db.auditLog.create({
+            data: {
+                orgId,
+                entityId,
+                entityType,
+                entityTitle
+                
+            }
+        })
+
+    } catch (error) {
+        console.log("[AUDIT_LOG_ERROR]", error);
+
+    }
+};
