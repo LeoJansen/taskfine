0204c70346c3dcecb261f65fbe669333ed09ed4b
Léo Jansen <leo@leoo.app>
2024-09-15 19:16:29 -0300

    Update route.ts

--- app/api/cards/[cardId]/logs/route.ts
+++ app/api/cards/[cardId]/logs/route.ts
@@ -1,4 +1,6 @@
+import { db } from "@/lib/db";
 import { auth } from "@clerk/nextjs/server";
+import { ENTITY_TYPE } from "@prisma/client";
 import { NextResponse } from "next/server";
 
 export async function GET(
@@ -10,8 +12,21 @@ export async function GET(
 
         if(!userId || !orgId) {
             return new NextResponse("Unauthorized", {status: 401})
+        };
 
-        }
+        const auditLogs = await db.auditLog.findMany({
+
+            where:{
+                orgId,
+                entityId: params.cardId,
+                entityType: ENTITY_TYPE.CARD,
+            },
+            orderBy: {
+                createdAt:"desc"
+            },
+            take:3,
+
+        })
 
     } catch (error) {
         return new NextResponse("Internal Error", { status: 500 });
