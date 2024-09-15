d2a804b9e50e63ea39292cf59637865b002c066f
Léo Jansen <leo@leoo.app>
2024-09-15 15:30:26 -0300

    Update schema.ts

--- actions/create-card/schema.ts
+++ actions/create-card/schema.ts
@@ -2,14 +2,13 @@ import { z } from "zod";
 
 
 export const CreateCard = z.object({
-    title: z.optional(
-        z.string({
-            required_error: "Title is required",
-            invalid_type_error: "Title is required",
-        }).min(3, {
-            message: "Title is too short",
-        })
-    ),
+    title: z.string({
+        required_error: "Title is required",
+        invalid_type_error: "Title is required",
+    }).min(3, {
+        message: "Title is too short",
+    })
+    ,
     boardId: z.string(),
     listId: z.string()
 });
