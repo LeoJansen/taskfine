3295963d6062225b265c96995157f1fdb410e85d
Léo Jansen <leo@leoo.app>
2024-09-15 15:24:50 -0300

    Update create-audit-log.ts

--- lib/create-audit-log.ts
+++ lib/create-audit-log.ts
@@ -21,7 +21,7 @@ export const CreateAuditLog = async ({
         const { orgId } = auth();
         const user = await currentUser();
 
-        if(!user || !orgId) {
+        if (!user || !orgId) {
             throw new Error("User not found!");
         };
 
@@ -30,10 +30,13 @@ export const CreateAuditLog = async ({
                 orgId,
                 entityId,
                 entityType,
-                entityTitle
-                
+                entityTitle,
+                action,
+                userId: user.id,
+                userImage: user?.imageUrl,
+                userName: user?.firstName + " " + user?.lastName
             }
-        })
+        });
 
     } catch (error) {
         console.log("[AUDIT_LOG_ERROR]", error);
