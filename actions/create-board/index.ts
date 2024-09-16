05ceeab208ce6566aad8919020a374ec8ef0a9d5
Léo Jansen <leo@leoo.app>
2024-09-15 21:31:44 -0300

    Update index.ts

--- actions/create-board/index.ts
+++ actions/create-board/index.ts
@@ -6,6 +6,8 @@ import { db } from "@/lib/db";
 import { revalidatePath } from "next/cache";
 import { createSafeAction } from "@/lib/create-safe-action";
 import { CreateBoard } from "./schema";
+import { CreateAuditLog } from "@/lib/create-audit-log";
+import { ACTION, ENTITY_TYPE } from "@prisma/client";
 
 const handler = async (data: InputType): Promise<ReturnType> => {
   const { userId, orgId } = auth();
@@ -56,6 +58,16 @@ const handler = async (data: InputType): Promise<ReturnType> => {
         imageUserName
       }
     });
+
+    await CreateAuditLog({
+      entityId: board.id,
+      entityTitle:board.title,
+      entityType: ENTITY_TYPE.BOARD,
+      action: ACTION.CREATE,
+    });
+
+
+
   } catch (error) {
     console.log("error returned")
     return {
