4f2c07cefe0cadd718bf16e38948abf90e473bef
Léo Jansen <leo@leoo.app>
2024-09-15 21:26:27 -0300

    Update index.ts

--- actions/copy-card/index.ts
+++ actions/copy-card/index.ts
@@ -6,6 +6,8 @@ import { db } from "@/lib/db";
 import { revalidatePath } from "next/cache";
 import { createSafeAction } from "@/lib/create-safe-action";
 import { CopyCard } from "./schema";
+import { CreateAuditLog } from "@/lib/create-audit-log";
+import { ACTION, ENTITY_TYPE } from "@prisma/client";
 
 
 const handler = async (data: InputType): Promise<ReturnType> => {
@@ -51,6 +53,13 @@ const handler = async (data: InputType): Promise<ReturnType> => {
                 order: newOrder,
                 listId: cardToCopy?.listId
             },
+        });
+
+        await CreateAuditLog({
+            entityTitle: card.id,
+            entityId: card.id,
+            entityType: ENTITY_TYPE.CARD,
+            action: ACTION.CREATE,
         });
 
 
