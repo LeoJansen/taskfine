c4678e6c39badc0dca028d182677ba025c72df83
Léo Jansen <leo@leonardojansen.com>
2024-09-17 19:01:56 -0300

    Update route.ts

--- app/api/cards/[cardId]/webhook/route.ts
+++ app/api/cards/[cardId]/webhook/route.ts
@@ -1,3 +1,4 @@
+import { db } from "@/lib/db";
 import { stripe } from "@/lib/stripe";
 import { headers } from "next/headers";
 import { NextResponse } from "next/server";
@@ -21,11 +22,36 @@ export async function POST(req: Request) {
     };
 
     const session = event.data.object as Stripe.Checkout.Session;
+
     if (event.type === "checkout.session.completed") {
         const subscription = await stripe.subscriptions.retrieve(
             session.subscription as string
         );
+
+
+        if (!session?.metadata?.orgId) {
+            return new NextResponse("Org ID is required", { status: 400 });
+        };
+
+        await db.orgSubscription.create({
+            data: {
+                orgId: session?.metadata?.orgId,
+                stripeSubscriptionId: subscription.id,
+                stripeCustomerId: subscription.customer as string,
+                stripePriceId: subscription.items.data[0].price.id,
+                stripeCurrentPeriodEnd: new Date(
+                    subscription.current_period_end * 1000
+                ),
+            },
+        });
+
+
+
+
     };
+
+
+
 
 
 
