b817d39859cb853998e6c52bc7cac8265e9c58e2
Léo Jansen <leo@leonardojansen.com>
2024-09-17 16:29:36 -0300

    Update index.ts

--- actions/stripe-redirect/index.ts
+++ actions/stripe-redirect/index.ts
@@ -1,6 +1,6 @@
 "use server"
 
-import { auth } from "@clerk/nextjs/server";
+import { auth, currentUser } from "@clerk/nextjs/server";
 import { InputType, ReturnType } from "./types";
 import { db } from "@/lib/db";
 import { revalidatePath } from "next/cache";
@@ -9,11 +9,13 @@ import { StripeRedirect } from "./schema";
 import { CreateAuditLog } from "@/lib/create-audit-log";
 import { ACTION, ENTITY_TYPE } from "@prisma/client";
 import { absoluteUrl } from "@/lib/utils";
+import { stripe } from "@/lib/stripe";
 
 
 const handler = async (data: InputType): Promise<ReturnType> => {
 
     const { userId, orgId } = auth();
+    const user = await currentUser();
 
     if (!userId || !orgId) {
         return {
@@ -29,6 +31,38 @@ const handler = async (data: InputType): Promise<ReturnType> => {
                 orgId,
             },
         });
+
+        if (orgSubscription && orgSubscription.stripeCustomerId) {
+            const stripeSession = await stripe.billingPortal.sessions.create({
+                customer: orgSubscription.stripeCustomerId,
+                return_url: settingsUrl,
+            });
+
+            url = stripeSession.url;
+
+        } else {
+            const stripeSession = await stripe.checkout.sessions.create({
+                success_url: settingsUrl,
+                cancel_url: settingsUrl,
+                payment_method_types: ["pix", "card"],
+                mode: "subscription",
+                billing_address_collection: "auto",
+                customer_email: user?.emailAddresses[0].emailAddress,
+                line_items: [
+                    {
+                        price_data: {
+                            currency: "USD",
+                            product_data: {
+                                name: "Taskifine Pro",
+                                description: "Unilimited boards for your organization."
+                            },
+                        },
+                    },
+                ],
+            });
+
+          
+        }
 
     } catch (error) {
 
