c654478b2a61124ee43174c47611bc4bfa50849e
Léo Jansen <leo@leoo.app>
2024-09-18 00:19:21 -0300

    Update info.tsx

--- app/(platform)/(dashboard)/organization/[organizationId]/_components/info.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/_components/info.tsx
@@ -5,14 +5,19 @@ import { useOrganization } from "@clerk/nextjs";
 import { CreditCard } from "lucide-react";
 import Image from "next/image";
 
+interface InfoProps {
+    isPro: boolean;
 
+};
 
-export const Info = () => {
+export const Info = ({
+    isPro
+}: InfoProps) => {
     const { organization, isLoaded } = useOrganization();
 
     if (!isLoaded) {
         return (
-            <Info.Skeleton/>
+            <Info.Skeleton />
         )
     }
 
@@ -41,22 +46,22 @@ export const Info = () => {
 };
 
 Info.Skeleton = function SkeletonInfo() {
-    return(
+    return (
         <div className="flex items-center gap-x-4">
             <div className="w-[60px] h-[60px] relative">
-                <Skeleton className="w-full h-full absolute"/>
+                <Skeleton className="w-full h-full absolute" />
             </div>
             <div className="space-y-2">
-                <Skeleton className="h-10 w-[200px]"/>
+                <Skeleton className="h-10 w-[200px]" />
             </div>
             <div className="flex items-center">
-                <Skeleton className="h-4 w-4 mr-2"/>
-                <Skeleton className="h-4 w-[100px]"/>
+                <Skeleton className="h-4 w-4 mr-2" />
+                <Skeleton className="h-4 w-[100px]" />
 
 
             </div>
 
         </div>
-    )
-}
+    );
+};
 
