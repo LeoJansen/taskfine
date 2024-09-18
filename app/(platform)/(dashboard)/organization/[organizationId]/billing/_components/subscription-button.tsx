4a39d55afefa0c6cd8d4d656e98bc721d00000e5
Léo Jansen <leo@leoo.app>
2024-09-18 11:44:48 -0300

    Implements Subscription Button

--- app/(platform)/(dashboard)/organization/[organizationId]/billing/_components/subscription-button.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/billing/_components/subscription-button.tsx
@@ -3,6 +3,8 @@
 import { stripeRedirect } from "@/actions/stripe-redirect";
 import { Button } from "@/components/ui/button";
 import { useAction } from "@/hooks/use-action";
+import { useProModal } from "@/hooks/use-pro-modal";
+import { toast } from "sonner";
 
 interface SubscriptionButtonProps {
     isPro: boolean;
@@ -12,11 +14,29 @@ interface SubscriptionButtonProps {
 export const SubscriptionButton = ({
     isPro
 }: SubscriptionButtonProps) => {
-    const {execute, isLoading} = useAction(stripeRedirect);
-    
+
+    const proModal = useProModal();
+    const {execute, isLoading} = useAction(stripeRedirect, {
+        onSuccess: (data)=> {
+            window.location.href = data;
+        },
+        onError(error) {
+            toast.error(error);
+        },
+    });
+
+    const onClick = () => {
+        if(isPro) {
+            execute({});
+        } else{
+            proModal.onOpen();
+        }
+    }
+
 
     return (
        <Button
+       onClick={onClick}
        variant="default"
        disabled={isLoading}
        >
