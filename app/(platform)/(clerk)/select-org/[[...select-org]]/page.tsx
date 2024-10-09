a8343656be736e3e5d78593e6bf7dde17b4fb905
Léo Jansen <leo@leoo.app>
2024-10-08 23:30:35 -0300

    Update page.tsx

--- app/(platform)/(clerk)/select-org/[[...select-org]]/page.tsx
+++ app/(platform)/(clerk)/select-org/[[...select-org]]/page.tsx
@@ -1,15 +1,15 @@
 import { OrganizationList } from "@clerk/nextjs";
 
-export default function CreateOrganizatioPage(){
+export default function CreateOrganizatioPage() {
     return (
         <div className="flex h-screen w-full justify-center items-center">
             <OrganizationList
-        hidePersonal
-        afterSelectOrganizationUrl="/organization/:id"
-        afterCreateOrganizationUrl="/organization/:id"
-        />
+                hidePersonal
+                afterSelectOrganizationUrl="/organization/:id"
+                afterCreateOrganizationUrl="/organization/:id"
+            />
 
         </div>
-        
+
     )
 }
