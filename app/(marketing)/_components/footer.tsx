527ccba58d9437c05c6a91da93106235f4325a3a
Léo Jansen <leo@leoo.app>
2024-10-12 16:40:41 -0300

    Implements logo

--- app/(marketing)/_components/footer.tsx
+++ app/(marketing)/_components/footer.tsx
@@ -1,5 +1,6 @@
 import Logo from '@/components/logo'
 import { Button } from '@/components/ui/button'
+import Image from 'next/image';
 import Link from 'next/link'
 import React from 'react'
 
@@ -8,6 +9,19 @@ export const Footer = () => {
     <div className='fixed bottom-0 w-full  p-4 border-t  bg-slate-100'>
       <div className='md:max-w-screen-2xl mx-auto flex justify-between w-full items-center'>
         <Logo />
+        <div className='flex  items-center'>
+          <p className='text-neutral-400 text-sm'>Developed by Léo Jansen</p>
+          <Button size="sm" variant="ghost" asChild>
+            <Link href="https://www.leonardojansen.com">
+              <Image src="/purpleleologo.svg" alt="logo" width={32} height={32} />
+            </Link>
+          </Button>
+
+        </div>
+     
+
+
+
         <div className="space-x-4 md:block md:w-auto flex items-center justify-between w-full">
           <Button size="sm" variant="ghost" asChild>
             <Link href="/sign-in">
@@ -20,8 +34,10 @@ export const Footer = () => {
               Terms of Service
             </Link>
           </Button>
+    
 
         </div>
+        
       </div>
     </div>
   );
